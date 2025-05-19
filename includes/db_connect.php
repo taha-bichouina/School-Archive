cc9c194eb72f901c3e1a2e8af9eb00760b7c601b
Taha Bichouina <136441006+taha-bichouina@users.noreply.github.com>
2025-05-19 18:38:42 +0100

    Add files via upload

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection settings for InfinityFree
-$host = "localhost"; // SQL Host provided by InfinityFree
-$username = "root";       // Database username
-$password = ""; // Database password
-$database = "school_archive";    // Database name
+$host = "sql110.infinityfree.com"; // SQL Host provided by InfinityFree
+$username = "if0_38512020";       // Database username
+$password = "owTQ6KFONPrDd"; // Database password
+$database = "if0_38512020_school_archive";    // Database name
 
 // Create connection
 $conn = new mysqli($host, $username, $password, $database);
